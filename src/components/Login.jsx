6adb73f67f3eb2e2ab8f19b42e484e21ad4008a1
seyedhassanhosseini <seyedhassan.hoseiny@gmail.com>
2023-05-02 20:55:06 +0200

    new

---
+++ src/components/Login.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useState } from "react";
+
+const Login = () => {
+  
+
+  return (
+    <div className="container">
+      <span className="user"></span>
+      <form>
+        <input
+          type="text"
+          
+        />
+        <input
+          type="password"
+         
+        />
+        <button >
+          Login
+        </button>
+        <span
+          data-t
+        >
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
